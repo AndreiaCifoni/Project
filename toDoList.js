93e8a5a21ec115a80176a06a5534fbbddc3638c8
Andreia Cifoni <andreiacifoni@gmail.com>
2020-11-29 10:21:33 -0500

    added function of list and new

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,18 @@
-let input = prompt("What would you like to do?")
-while (input !== "quit") {
+let input = prompt("What would you like to do?");
+const todo = ["clean litter","clean house"];
+while (input !== "quit" && input !== "q") {
+    if(input === "list") {
+        console.log("**********")
+        for (let i = 0; i < todo.length; i++) {
+            console.log(`${i}: ${todo[i]}`);
+        }
+        console.log("**********")
+    } else if (input === "new") {
+        const newTodo = prompt("Ok, what is the new todo?");
+        todo.push(newTodo);
+        console.log(`${newTodo} added to the list`)
+    }
     input = prompt("What would you like to do?");
-} else {
-    console.log("OK, you quit!");
-} 
+}
+console.log("OK, you quit!");
+ 
