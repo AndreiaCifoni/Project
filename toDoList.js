9840a673aa4f9b3e4a72b227632290df5893fad6
Andreia Cifoni <andreiacifoni@gmail.com>
2020-11-29 10:50:22 -0500

    function delete added

--- toDoList.js
+++ toDoList.js
@@ -1,7 +1,7 @@
 let input = prompt("What would you like to do?");
 const todo = ["clean litter","clean house"];
 while (input !== "quit" && input !== "q") {
-    if(input === "list") {
+    if (input === "list") {
         console.log("**********")
         for (let i = 0; i < todo.length; i++) {
             console.log(`${i}: ${todo[i]}`);
@@ -11,6 +11,14 @@ while (input !== "quit" && input !== "q") {
         const newTodo = prompt("Ok, what is the new todo?");
         todo.push(newTodo);
         console.log(`${newTodo} added to the list`)
+    } else if (input === "delete") {
+        const index = parseInt(prompt("Ok, enter an index to delete:"));
+        if( !Number.isNaN(index)) {
+            const deleted = todo.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log("Unknow index")
+        }
     }
     input = prompt("What would you like to do?");
 }
